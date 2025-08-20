9f4e42ba0d1629e9de722466f5b1f59aa456058a
Papp Dávid <impappdavid@gmail.com>
2025-08-20 14:40:00 +0200

    Adding accordion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Filter from './components/mycomponents/filter'
+import Navbar from './components/mycomponents/navbar'
 
 function App() {
 
   return (
     <>
-      
+      <div className="bg-[#FFF5EE] w-full min-h-screen">
+          <Navbar />
+          <Filter />
+      </div>
     </>
   )
 }
